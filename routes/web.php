de248f073c7e2c5f8ad646389fdc0d053c208d0b
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-09-14 17:36:05 +0200

    fine esercizio

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $saluti = ['ciao', 'hello', 'salut'];
+    return view('home', compact('saluti'));
 });
